9ac2742d52e4373f787509d16ddfac41cf1b37e9
Matt Dunn <mattdunn0816@gmail.com>
2024-10-18 18:27:12 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Flex Mobile App
---
+++ scripts/app_release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Commit and tag this change.
+version=`grep 'version: ' pubspec.yaml | sed 's/version: //'`
+
+git config --global user.email "83490846+github-actions[bot]@users.noreply.github.com"
+git config --global user.name "github-actions[bot]"
+
+git add ./pubspec.yaml
+git commit -m "Bump version to $version" ./pubspec.yaml
+git push
+
+git tag $version
+git push --tags
